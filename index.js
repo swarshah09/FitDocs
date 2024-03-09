d9d3b78c09d7eae715ec9bb022fc546043d9b275
swarshah09 <90791181+swarshah09@users.noreply.github.com>
2024-03-09 16:19:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the button element
+    var navigateButton = document.getElementById('navigateButton');
+    
+    // Add click event listener to the button
+    navigateButton.addEventListener('click', function() {
+        // Navigate to the other HTML page
+        window.location.href = '/nutrition.html'; // Replace 'other_page.html' with the actual URL of the other HTML page
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the button element
+    var navigateButton2 = document.getElementById('navigateButton2');
+    
+    // Add click event listener to the button
+    navigateButton2.addEventListener('click', function() {
+        // Navigate to the other HTML page
+        window.location.href = '/pharmacology.html'; // Replace 'other_page.html' with the actual URL of the other HTML page
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the button element
+    var navigateButton3 = document.getElementById('navigateButton3');
+    
+    // Add click event listener to the button
+    navigateButton3.addEventListener('click', function() {
+        // Navigate to the other HTML page
+        window.location.href = '/encyclopedia.html'; // Replace 'other_page.html' with the actual URL of the other HTML page
+    });
+});
